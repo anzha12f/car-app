e44ac9e8e8d299c66c87b62c0481401929c2f3f0
AijazA <anzha12f@gmail.com>
2023-09-11 08:45:03 +1000

    Start Client commit-3

--- client/car-app-client/src/app/app.module.ts
+++ client/car-app-client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarSearchComponent } from './car-search/car-search.component';
+import { CarSearchHeaderComponent } from './car-search-header/car-search-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarSearchComponent
+    CarSearchComponent,
+    CarSearchHeaderComponent
   ],
   imports: [
     BrowserModule,
